585f542fc2186eac39190cae03a0f739928cfd35
Shreejith SG <shreejithsg@gmail.com>
2024-04-08 12:57:51 +0530

    Update gcv.py

--- gcv.py
+++ gcv.py
@@ -9,7 +9,7 @@ import os
 
 load_dotenv()
 service_account_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'trueinfolabs-ocr-20c8c095084b.json'
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'gcv-new-project-dd6ed833cc91.json'
 
 client = vision.ImageAnnotatorClient() 
 
